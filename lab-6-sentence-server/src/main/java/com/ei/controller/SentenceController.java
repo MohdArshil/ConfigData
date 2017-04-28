298b3046cd1cccdd4366e8c5770d3c4e48becdc0
arshil <mohdarshi.shaikh@einfochips.com>
2017-04-28 09:25:15 +0530

    Initial commit

---
+++ lab-6-sentence-server/src/main/java/com/ei/controller/SentenceController.java
@@ -0,0 +1,26 @@
+package com.ei.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ei.service.SentenceService;
+
+@Controller
+public class SentenceController {
+
+	@Autowired
+	SentenceService sentenceService;
+
+	/**
+	 * Display a small list of Sentences to the caller:
+	 */
+	@RequestMapping("/sentence")
+	public @ResponseBody String getSentence() {
+		return "<h3>Some Sentences</h3><br/>" + sentenceService.buildSentence() + "<br/><br/>"
+				+ sentenceService.buildSentence() + "<br/><br/>" + sentenceService.buildSentence() + "<br/><br/>"
+				+ sentenceService.buildSentence() + "<br/><br/>" + sentenceService.buildSentence() + "<br/><br/>";
+	}
+
+}
